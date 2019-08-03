e31193cdb39858f66a0ab83217b9c8a6650e6ec8
BuildTools <unconfigured@null.spigotmc.org>
2019-08-04 01:15:24 +0800

    Finished Prototype

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public float FollowSpeed = 120f;
+    public Transform Target;
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+        Vector3 newPosition = Target.position;
+        newPosition.z = -10;
+        transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * 4.5f * Time.deltaTime);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform player;
 
- 
     // Start is called before the first frame update
     void Start()
     {
@@ -15,22 +15,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.x--;
-            this.transform.position = position;
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.x++;
-            this.transform.position = position;
-        }
 
+
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                Vector3 position = this.transform.position;
+                position.x--;
+                this.transform.position = position;
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                Vector3 position = this.transform.position;
+                position.x++;
+                this.transform.position = position;
+            }
+    
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Physics.gravity = Vector3.down*9.81f;
+            
         }
     }
 }
