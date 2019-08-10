4bbf36ca63e82c855ffba6201cee1c33585c33d6
BuildTools <unconfigured@null.spigotmc.org>
2019-08-11 00:36:53 +0800

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Transform player;
-
+    // This is so that you cannot move mid air
+    public bool canMove;
     // Start is called before the first frame update
     void Start()
     {
         //By default gravity is turned off for the object
         Physics.gravity = Vector3.zero;
+        canMove = true;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //When space is pressed gravity is turned on
+            Physics.gravity = Vector3.down * 9.81f;
+            canMove = false;
+        }
 
 
-            //This checks to see if the left or right arrow key is pressed to then move them in the respective direction
+        // This only allows left and right movement if canMove is true
+        if (canMove == true){
+            // This checks to see if the left or right arrow key is pressed to then move them in the respective direction
             if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
             {
                 Vector3 position = this.transform.position;
@@ -39,12 +48,7 @@ public class PlayerController : MonoBehaviour
                 }
                 this.transform.position = position;
             }
-    
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //When space is pressed gravity is turned on
-            Physics.gravity = Vector3.down*9.81f;
-            
+
         }
     }
 }
